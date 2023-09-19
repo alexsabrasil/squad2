1830abdcc65b0f0b0c463d016ebf3e479e8e9244
godoidotjs <rpgodoisantos@gmail.com>
2023-09-19 10:04:22 -0300

    trabalho em grupo squad2

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
+// Imagine que você está desenvolvendo um sistema para uma loja de eletrônicos.
+// Você precisa criar um programa simples em JavaScript para armazenar informações
+// sobre os clientes da loja. Cada cliente tem um nome (string), uma idade (number) 
+// e um status de membro (bool).
 
+const nomeCliente1 = "Alberto";
+const idadeCliente1 = 15;
+const eMembro1 = true;
+
+console.log("Cliente 1:");
+console.log("Nome: " + nomeCliente1);
+console.log("Idade: " + idadeCliente1);
+console.log("Membro: " + eMembro1);
+
+
+const nomeCliente2 = "Maria";
+const idadeCliente2 = 30;
+const eMembro2 = false;
+
+const nomeCliente3 = "Alberto";
+const idadeCliente3 = 15;
+const eMembro3 = true
+
+console.log("Cliente 1:");
+console.log("Nome: " + nomeCliente);
+console.log("Idade: " + idadeCliente);
+console.log("Membro: " + eMembro);
+
+const nomeCliente = prompt("Digite o nome do cliente aqui: ");
+console.log("nome:" = nomeCliente)
+const idadeCliente = prompt("Digite a idade do cliente aqui: ");
+console.log("idade:",idadeCliente)
+const eMembro = true;
+console.log("status: " + eMembro);
+
+
+// git checkout -b nome-da-sua-branch
+// git add .
+// git commit -m "Atualização das informações do cliente"
+// git push origin nome-da-sua-branch
