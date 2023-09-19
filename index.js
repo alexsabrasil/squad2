482ffda4d40ff11ad2f19ea05fe83527e25042db
Guilherme220294 <guilhermedosreissilva@yahoo.com.br>
2023-09-19 20:26:21 -0300

    modificações

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+/*Imagine que você está desenvolvendo um sistema para uma loja de eletrônicos.
+Você precisa criar um programa simples em JavaScript para armazenar informações
+sobre os clientes da loja. Cada cliente tem um nome (string), uma idade (number) 
+e um status de membro (bool).*/
 
+const nomeCliente = 'Gabriel';
+const idadeCliente = 33;
+const eMembro = true;
+
+
+
+
+ ~~~javascript
+Cliente 1: 
+nome: "Alberto"
+idade: 15
+status: true;
+~~~
+
+
+
+
+- Crie três constantes: nomeCliente, idadeCliente e eMembro para armazenar informações sobre um cliente específico.
+- Certifique-se de usar o tipo de dado apropriado para cada constante (string, number, bool).
+
+- Agora, cada integrante do grupo precisará alterar os valores das constantes para representar outro funcionário
+e exibir as informações desse novo funcionário no console.
+
+- Assim que isso for feito, voce deverá criar uma branch e enviar as atualizacoes para a sua respectiva branch. 
+
+Feito isso, voce deverá criar um Pull request endereçado a `main/master` do projeto.
